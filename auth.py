82305b0eabd084b6b9f1a9237a3f7748398fd196
fazil1406 <mf3462354@gmail.com>
2024-02-17 23:03:59 +0530

    heloo

--- auth.py
+++
@@ -1,9 +0,0 @@
-import demo
-db=demo.client['meeting']
-collection=db['room']
-dictionary={
-    'name':'fazil',
-    'marks':56
-}
-
-collection.insert_one(document=dictionary)
--- demo.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-client=pymongo.MongoClient("mongodb+srv://meet:jpAPz72rb5fEsw1Q@cluster1.bwo1rzz.mongodb.net/?retryWrites=true&w=majority")
-
-
-    
-
