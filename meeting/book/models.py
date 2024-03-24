14c10233f9ec34902ed7fea604a47631adfd86a7
fazil1406 <mf3462354@gmail.com>
2024-03-24 17:05:02 +0530

    book database

--- meeting/book/models.py
+++ meeting/book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    host_name=models.CharField(max_length=200)
+    class_name=models.CharField(max_length=100)
+    students=models.IntegerField()
+    subject=models.CharField(max_length=100)
+    timing=models.TimeField(auto_now=False, auto_now_add=False,)
+
+    class Meta:
+        db_table="Booking"
--- meeting/book/views.py
+++ meeting/book/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,UserManager,auth
+from django.contrib.auth.decorators import login_required
+from .models import Booking
 
 # Create your views here.
+@login_required
 def meeting(request):
-    return render(request,'meeting.html')
+       if request.method=='POST':
+         hostname=request.POST.get('host_name')
+         classname=request.POST.get('class_name')
+         students=request.POST.get('no_of_students')
+         subject=request.POST.get('subject')
+         timing=request.POST.get('timing')
+         user=request.user.id
+         booking = Booking(user, host_name=hostname, class_name=classname, students=students,subject=subject,timing=timing)
+         booking.save()
+         return redirect('/home')
+       else:  
+        return render(request,'meeting.html')
